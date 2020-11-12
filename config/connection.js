88ec2dff677b751b5a7c581f062f6525038ef1a1
Luis <lgd26308@gmail.com>
2020-11-11 22:35:54 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ let connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-
     connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
@@ -14,7 +13,7 @@ if (process.env.JAWSDB_URL) {
         password: "root",
         database: "burgers_db",
     });
-}
+};
 
 // Make connection.
 connection.connect(function (err) {
