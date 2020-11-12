793a945cc6cc20b8ebf620451a6d0459f1bb822b
Luis <lgd26308@gmail.com>
2020-11-11 21:20:33 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 
 // Set up MySQL connection.
 const mysql = require("mysql");
-let connection
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
